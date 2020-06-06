1213092194080cd44e9cff476239c28fe7dc3151
tai-fujio <51994563+tai-fujio@users.noreply.github.com>
2020-06-06 20:28:08 +0900

    useEffect 外部api

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import CounterHooks from './components/CounterHooks';
 import FormHook from './components/FormHook';
 import ItemHook from './components/ItemHook';
 import MouseEventEffect from './components/MouseEventEffect';
+import DataFetch from './components/DataFetch';
 function App() {
   return (
     <div className="App">
+      <DataFetch />
       <MouseEventEffect />
       <CounterHooks />
       {/* <FormHook /> */}
