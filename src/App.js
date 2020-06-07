cffaa48c621b86601afa22f71967817f7bb2a53d
tai-fujio <51994563+tai-fujio@users.noreply.github.com>
2020-06-07 16:31:51 +0900

    useContext 複数state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {createContext,useState} from 'react';
+import {createContext,useState,useReducer} from 'react';
 import './App.css';
 import Counter from './components/Counter';
 import CounterHooks from './components/CounterHooks';
@@ -10,10 +10,30 @@ import DataFetch from './components/DataFetch';
 import DataFetchId from './components/DataFetchId';
 import ComponentC from './components/ComponentC';
 import CounterReducer from './components/CounterReducer';
+import ComponentX from './components/ComponentX';
+import ComponentY from './components/ComponentY';
+import ComponentZ from './components/ComponentZ';
 export const UserInfo = createContext({});
 export const LanguageInfo = createContext();
+export const StatusContext = createContext({});
+
+const initialState = {HP: 0, MP: 0}
+const globalReducer = (state,action)=> {
+  switch(action.type){
+    case 'increment':
+      return {...state, HP : state.HP + 10}
+    case 'decrement':
+      return {...state, MP : state.MP + 10}
+    case 'reset':
+      return initialState
+    default:
+      return state
+  }
+}
+
 function App() {
-  const [user,setUser] = useState({name:'fujio',age:'33'});
+  const [status,dispatch] = useReducer(globalReducer,initialState)
+  const [user,setUser] = useState({name:'fujio',age:'33'})
   const [language,setLanguage] = useState('Japanese')
   return (
     <div className="App">
@@ -29,6 +49,13 @@ function App() {
       {/* <FormHook /> */}
       {/* <ItemHook /> */}
       <CounterReducer/>
+      <h1>{status.HP}/{status.MP}</h1>
+      <StatusContext.Provider value = {{dispatch:dispatch,status:status}}>
+      {/* <StatusContext.Provider value = {{dispatch:dispatch,status:status}}> */}
+        <ComponentX />
+        <ComponentY />
+        <ComponentZ />
+      </StatusContext.Provider>
 
     </div>
   );
---
+++ src/components/ComponentY.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {useContext} from 'react'
+import {StatusContext} from '../App'
+function ComponentY() {
+  const status = useContext(StatusContext)
+  return(
+    <div>
+      <button onClick={()=>status.dispatch({type:'increment',value:10})}>HP</button>
+      <button onClick={()=>status.dispatch({type:'decrement',value:10})}>MP</button>
+      <button onClick={()=>status.dispatch({type:'reset'})}>Reset</button>
+    </div>
+  )
+}
+
+export default ComponentY
