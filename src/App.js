4902f53ab80d490713285f57b217cbed956a160a
tai-fujio <51994563+tai-fujio@users.noreply.github.com>
2020-06-07 16:48:19 +0900

    reducer count値の動的化/useEffect 外部API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {createContext,useState,useReducer} from 'react';
+import {createContext,useState,useReducer,useEffect} from 'react';
 import './App.css';
 import Counter from './components/Counter';
 import CounterHooks from './components/CounterHooks';
@@ -13,17 +13,23 @@ import CounterReducer from './components/CounterReducer';
 import ComponentX from './components/ComponentX';
 import ComponentY from './components/ComponentY';
 import ComponentZ from './components/ComponentZ';
+import axios from 'axios'
 export const UserInfo = createContext({});
 export const LanguageInfo = createContext();
 export const StatusContext = createContext({});
 
+const apiInitialState = {
+  loading: true,
+  error: '',
+  post: {},
+}
 const initialState = {HP: 0, MP: 0}
 const globalReducer = (state,action)=> {
   switch(action.type){
     case 'increment':
-      return {...state, HP : state.HP + 10}
+      return {...state, HP : state.HP + action.value}
     case 'decrement':
-      return {...state, MP : state.MP + 10}
+      return {...state, MP : state.MP + action.value}
     case 'reset':
       return initialState
     default:
@@ -31,10 +37,40 @@ const globalReducer = (state,action)=> {
   }
 }
 
+  const apiReducer = (state,action) => {
+    switch(action.type){
+      case 'Success':
+        return {
+          loading: false,
+          post: action.payload,
+          error: '',
+        }
+      case 'Error':
+        return {
+          loading: false,
+          post: {},
+          error: 'failed to fetch data',
+        }
+      default:
+        return state
+    }
+  }
 function App() {
+  const [apiState,apiDispatch] = useReducer(apiReducer,apiInitialState)
   const [status,dispatch] = useReducer(globalReducer,initialState)
   const [user,setUser] = useState({name:'fujio',age:'33'})
   const [language,setLanguage] = useState('Japanese')
+  useEffect(()=>{
+    axios
+      .get('https://jsonplaceholder.typicode.com/posts/1')
+      
+      .then(res => {
+        apiDispatch({type:'Success',payload:res.data});
+      })
+      .catch(err => {
+        apiDispatch({type:'Error'})
+      })
+  },[])
   return (
     <div className="App">
       <UserInfo.Provider value={user}>
@@ -56,7 +92,8 @@ function App() {
         <ComponentY />
         <ComponentZ />
       </StatusContext.Provider>
-
+      <h1>{apiState.loading? 'Now Loading...':apiState.post.body}</h1>
+      <h2>{apiState.error?apiState.error:'Nothing Error'}</h2>
     </div>
   );
 }
